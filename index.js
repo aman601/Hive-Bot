fc3f49d604ca9affa98f30aa8b45275da40ed32b
Brent Thurber <thurberb@huntington.edu>
2021-01-06 20:19:01 -0500

    Bot now goes online when running node index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
 
+client.login(config.token);
+
+client.on('ready', () => {
+    console.log(`Logged in as: ${client.user.tag}`);
+});
+
+client.on('disconnect', () => {
+    console.log('Disconnected, check your internet connection.');
+});
+
+const prefix = 'h.';
